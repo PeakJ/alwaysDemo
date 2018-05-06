c6a77a6d27a23202449188fa18be48e35e618d91
PeakJ <jfup3336@gmail.com>
2018-05-06 17:56:19 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,35 @@
+function log(params) {
+	console.log(params);
+}
+
+
+
+
+var constru = new Date();
+// console.log(Object.prototype);
+// console.log(Object.prototype.__proto__);
+// console.log(Function.prototype);
+// console.log(Function.__proto__);
+// console.log(Function.prototype.__proto__);
+
+var num = 666 + 2;
+// console.log(num.__proto__);
+// console.log(Function.prototype === Function.__proto__);
+
+var foo = function testPrototype() {
+    console.log(...arguments);
+}
+// console.log(foo.__proto__);
+// console.log(foo.prototype);
+// console.log(foo('ljgl', 666, null));
+
+var bb = {
+    name: 'jiangfeng',
+    age: 26
+};
+// console.log(bb.prototype);
+var cc = {
+	name: 'jiangfeng',
+	age: 26
+};
+log(bb === cc); // true
