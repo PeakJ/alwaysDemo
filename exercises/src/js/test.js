b78b47aacc41f784449d4775ad22602052363738
jiangfeng <jiangfeng@yongche.com>
2018-12-07 14:29:33 +0800

    增加h5复制剪切功能

--- exercises/src/js/test.js
+++ exercises/src/js/test.js
@@ -1,43 +1,9 @@
-function log(params) {
-    console.log(params);
-}
-
-
 window.testJsonp = function(data){
     console.log('调用了');
     console.log(data);
     
 }
-
-// var constru = new Date();
-// console.log(Object.prototype);
-// console.log(Object.prototype.__proto__);
-// console.log(Function.prototype);
-// console.log(Function.__proto__);
-// console.log(Function.prototype.__proto__);
-
-// var num = 666 + 2;
-// console.log(num.__proto__);
-// console.log(Function.prototype === Function.__proto__);
-
-// var foo = function testPrototype() {
-//     console.log(...arguments);
-// }
-// console.log(foo.__proto__);
-// console.log(foo.prototype);
-// console.log(foo('ljgl', 666, null));
-
-// var bb = {
-//     name: 'jiangfeng',
-//     age: 26
-// };
-// // console.log(bb.prototype);
-// var cc = {
-// 	name: 'jiangfeng',
-// 	age: 26
-// };
-// log(bb === cc); // true
-
+ 
 // https://www.jianshu.com/u/6d92660a8cea?name=jiangfeng&age=18&sex=male
 function test(url,key){
     var reg = new RegExp("(^|&)"+ key +"=([^&]*)(&|$)");
@@ -50,3 +16,18 @@ function test(url,key){
 
 }
 console.log(test('https://www.jianshu.com/u/6d92660a8cea?name=peakj&age=18&sex=male','name'));
+
+/******业务代码 *******/
+var clipboard = new ClipboardJS('#copyBtn');
+clipboard.on('success', function(e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+
+    e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+});
